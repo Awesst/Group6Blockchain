51721df9892702e44b797016129cb36a59841f29
Katalin Widén <katalin.widen@medieinstitutet.se>
2022-12-21 03:48:27 +0100

    debounce corrected. now it won't hammer api
    it also stores latitude and longitude in localStorage --> easy to add to new block

--- src/api/locationScript.js
+++ src/api/locationScript.js
@@ -1,6 +1,6 @@
 export function rollApi(searchButton, input) {
 
-    // function that converts ipv4 IP address to approx. location and coordinates
+    // function that converts IPv4 IP address to approx. location and coordinates
     function geoLocate() {
 
         let ip = input.value;
@@ -20,6 +20,9 @@ export function rollApi(searchButton, input) {
                 locationInfo.textContent = location;
                 latitudeInfo.textContent = data.lat;
                 longitudeInfo.textContent = data.lon;
+
+                localStorage.setItem("latitude", data.lat);
+                localStorage.setItem("longitude", data.lon);
             })
             .catch(err => console.log(err));
     }
@@ -27,66 +30,67 @@ export function rollApi(searchButton, input) {
     // call GeoCoding API: Converts given address into lat/long; autocompletes to best match
     function geoCode() {
 
-        input.addEventListener((input), () => {
+        let timer; // debouncing
 
-            const inputAddress = debounce(input.value);      // debouncing input: see debounce()
-            const myAPIKey = "c5bc2c56928c4feb80c40df48fe1426c";
-           
-            const geocodingURL = `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(inputAddress)}&apiKey=${myAPIKey}`;
+        input.addEventListener("input", () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
 
-            // Call Geocoding API - https://www.geoapify.com/geocoding-api/
-            fetch(geocodingURL).then(result => result.json())
-                .then(featureCollection => {
-                    console.log(featureCollection);
-                    const foundAddress = featureCollection.features[0];
-                    let location = foundAddress.properties.city + ", " + foundAddress.properties.country;
+            timer = setTimeout(() => {
+                const inputAddress = input.value;
+                const myAPIKey = "1bdf6769d5f44e10b1c2bba7b8fe6844";
+                const geocodingURL = `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(inputAddress)}&apiKey=${myAPIKey}`; // The encodeURIComponent function is used to encode the inputAddress variable as a URL component, which is necessary to properly pass it in the URL
 
-                    let ipInfo = document.getElementsByClassName('ipInfo')[0];
-                    let locationInfo = document.getElementsByClassName('locationInfo')[0];
-                    let latitudeInfo = document.getElementsByClassName('latitudeInfo')[0];
-                    let longitudeInfo = document.getElementsByClassName('longitudeInfo')[0];
+                fetch(geocodingURL)
+                    .then((result) => result.json())
+                    .then((featureCollection) => {
+                        console.log(featureCollection);
+                        const foundAddress = featureCollection.features[0];
+                        let location = foundAddress.properties.city + ", " + foundAddress.properties.country;
 
-                    ipInfo.textContent = ""; // to empty IP field when not known
-                    locationInfo.textContent = location;
-                    latitudeInfo.textContent = foundAddress.properties.lat;
-                    longitudeInfo.textContent = foundAddress.properties.lon;
-                })
-                .catch(err => console.log(err));
-        })
+                        let ipInfo = document.getElementsByClassName("ipInfo")[0];
+                        let locationInfo = document.getElementsByClassName("locationInfo")[0];
+                        let latitudeInfo = document.getElementsByClassName("latitudeInfo")[0];
+                        let longitudeInfo = document.getElementsByClassName("longitudeInfo")[0];
+
+                        ipInfo.textContent = ""; // to empty IP field when not known
+                        locationInfo.textContent = location;
+                        latitudeInfo.textContent = foundAddress.properties.lat;
+                        longitudeInfo.textContent = foundAddress.properties.lon;
+
+                        localStorage.setItem("latitude", foundAddress.properties.lat);
+                        localStorage.setItem("longitude", foundAddress.properties.lon);
+
+                    })
+                    .catch((err) => console.log(err));
+            }, 1000); // the debouncing interval, in this case 1 second
+        });
     }
 
-    searchButton.addEventListener('click', () => {
+    searchButton.addEventListener("click", () => {
 
-        const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
-        const ipFormat = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
+        // regular expression that is used to match an IPv4 address
+        const isValidIp = value => (/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(value) ? true : false);
 
-        // keep this for now for more possible checks. 
-        //const isValidIp = value => (/^(?!\.)((^|\.)([1-9]?\d|1\d\d|2(5[0-5]|[0-4]\d))){4}$/.test(value) ? true : false);
-
+        // Check if the input element is empty
         if (!input.value) {
             geoLocate();
         }
 
-        else if (input.value !== ipFormat && input.value) {
+        // input is a valid IPv4 address --> geoLocate()
+        else if (isValidIp(input.value)) {
+            geoLocate();
+        }
+
+        // input is not a valid IPv4 address --> assume it's an address --> geoCode()
+        else if (!isValidIp(input.value)) {
             geoCode();
         }
 
         else {
-            alert("Make sure you entered a valid city/country");
-            // currently since the second APi autocompletes, we never end up here. I kept it for user-entered IP address that is invalid. Keep it for now, just to be able to close the if statement. Format as well, don't do just alert
+            ipInfo.textContent = "Make sure you entered a valid address";
+            // as the second API autocompletes wery widely, we pretty much never end up here. Kept it as placeholder just to close the statement.
         }
-
     })
-}
-
-// debouncing: eliminating unwanted signals from an input, so that it doesn't send a query after every key pressed, but with a delay in [ms]
-
-function debounce(func, delay = 1000) {
-    let timerId;
-    return (...args) => {
-        clearTimeout(timerId);
-        timerId = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
 }
